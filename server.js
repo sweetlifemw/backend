79cbcdc466e51e54c8225679ae27df680f3d42f2
sweetlifemw <sweetlifemw6@gmail.com>
2025-07-18 09:55:36 -0700

    server.js

--- server.js
+++ server.js
@@ -16,11 +16,13 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 //  Connect to MongoDB
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log(" MongoDB connected"))
-  .catch((err) => console.error(" MongoDB connection error:", err));
+mongoose.connect( MONGODB_URI)
+  .then(() => console.log(" MongoDB connected"))
+  .catch(err => {
+    console.error(" MongoDB connection error:");
+    console.error(err);
+  });
+
 
 
 // Schema
