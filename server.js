0a953bf89b3148baa0781fdb907b24b5eadffb68
sweetlifemw <sweetlifemw6@gmail.com>
2025-07-18 11:04:06 -0700

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const dotenv = require("dotenv").config()
 
 
 const app = express();
