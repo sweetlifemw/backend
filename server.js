02d3b603915374f105f3b5b3c9a4ff67f94406d3
sweetlifemw <sweetlifemw6@gmail.com>
2025-07-18 08:07:42 -0700

    server.js

--- server.js
+++ server.js
@@ -174,5 +174,6 @@ setInterval(() => {
 
 
 // Start
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
