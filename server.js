ee87c25ba464744aafc3ebad8380e7124e09f525
sweetlifemw <sweetlifemw6@gmail.com>
2025-07-19 06:00:29 -0700

    server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",              // VS Code Live Server
   "http://localhost:5500",              // Alternative
   "http://192.168.197.231:5500",        // Your actual IP for phone access
-  "https://<yourusername>.github.io",   // Replace with your GitHub Pages URL
+  "https://sweetlifemw.github.io",   // Replace with your GitHub Pages URL
   "https://sweetlife-1dpx.onrender.com" // Your backend URL (for Render)
 ];
 
