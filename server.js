a83132a2f8b354c28ca68d352c62c1a695be5293
sweetlifemw <sweetlifemw6@gmail.com>
2025-07-19 05:27:21 -0700

    server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "https://yourfrontend.com"],  // add exact origin
+  origin: "*",  // add exact origin
   credentials: true
 }));
 
